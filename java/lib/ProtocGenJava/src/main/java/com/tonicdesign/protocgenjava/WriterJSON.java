f66870d8cf9a25cb2d7ebba078f8498aa5582263
Brad Morgan <bmorgan@tonicdesign.com>
2015-05-19 15:13:38 -0400

    Cleanup imports

--- java/lib/ProtocGenJava/src/main/java/com/tonicdesign/protocgenjava/WriterJSON.java
+++ java/lib/ProtocGenJava/src/main/java/com/tonicdesign/protocgenjava/WriterJSON.java
@@ -5,8 +5,6 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.nio.charset.Charset;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Map;
 import java.util.Stack;
 
