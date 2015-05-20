226c45fa786962fa3c13f3f4c4cf27ced2071cdb
Brad Morgan <bmorgan@tonicdesign.com>
2015-05-20 11:39:46 -0400

    Refactoring

--- java/lib/ProtocGenJava/src/test/java/com/tonicdesign/protocgenjava/ReaderJSONTest.java
+++ java/lib/ProtocGenJava/src/test/java/com/tonicdesign/protocgenjava/ReaderJSONTest.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 
 import java.nio.charset.Charset;
 import java.util.HashMap;
+import java.util.Map;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNull;
@@ -19,7 +20,15 @@ public class ReaderJSONTest {
     private static final int TEST_TAG_NESTED = 30;
     private static final String TEST_TAG_NESTED_JSON_KEY = "testTagNested";
 
-    private ReaderJSON mReader;
+    private static final Map<String, Reader.TagMapValue> TAG_MAP;
+    private static final Map<String, Reader.TagMapValue> TAG_MAP_REPEATED;
+
+    static {
+        TAG_MAP = new HashMap<String, Reader.TagMapValue>();
+        TAG_MAP.put(TEST_TAG_JSON_KEY, new Reader.TagMapValue(TEST_TAG, false));
+        TAG_MAP_REPEATED = new HashMap<String, Reader.TagMapValue>();
+        TAG_MAP_REPEATED.put(TEST_TAG_REPEATED_JSON_KEY, new Reader.TagMapValue(TEST_TAG_REPEATED, true));
+    }
 
     @Test
     public void testInvalidJSON() throws Exception {
@@ -29,256 +38,192 @@ public class ReaderJSONTest {
 
     @Test
     public void testReadByte() throws Exception {
-        mReader = ReaderJSON.fromBuffer(bytesFromJSON("{\"testTag\": 250}"));
-        mReader.pushTagMap(new HashMap<String, Reader.TagMapValue>() {{
-            put(TEST_TAG_JSON_KEY, new Reader.TagMapValue(TEST_TAG, false));
-        }});
-        assertEquals(TEST_TAG, mReader.readTag());
-        assertEquals((byte) 0xFA, mReader.readByte());
+        Reader reader = getReader("{\"testTag\": 250}", TAG_MAP);
+        assertEquals(TEST_TAG, reader.readTag());
+        assertEquals((byte) 0xFA, reader.readByte());
     }
 
     @Test
     public void testReadVarInt64Bits() throws Exception {
-        mReader = ReaderJSON.fromBuffer(bytesFromJSON("{\"testTag\": 2886807498600235000}"));
-        mReader.pushTagMap(new HashMap<String, Reader.TagMapValue>() {{
-            put(TEST_TAG_JSON_KEY, new Reader.TagMapValue(TEST_TAG, false));
-        }});
-        assertEquals(TEST_TAG, mReader.readTag());
-        assertEquals(2886807498600235000L, mReader.readVarInt());
+        Reader reader = getReader("{\"testTag\": 2886807498600235000}", TAG_MAP);
+        assertEquals(TEST_TAG, reader.readTag());
+        assertEquals(2886807498600235000L, reader.readVarInt());
     }
 
     @Test
     public void testReadVarUInt64Bits() throws Exception {
-        mReader = ReaderJSON.fromBuffer(bytesFromJSON("{\"testTag\": 2886807498600235000}"));
-        mReader.pushTagMap(new HashMap<String, Reader.TagMapValue>() {{
-            put(TEST_TAG_JSON_KEY, new Reader.TagMapValue(TEST_TAG, false));
-        }});
-        assertEquals(TEST_TAG, mReader.readTag());
-        assertEquals(2886807498600235000L, mReader.readVarUInt());
+        Reader reader = getReader("{\"testTag\": 2886807498600235000}", TAG_MAP);
+        assertEquals(TEST_TAG, reader.readTag());
+        assertEquals(2886807498600235000L, reader.readVarUInt());
     }
 
     @Test
     public void testReadBool() throws Exception {
-        mReader = ReaderJSON.fromBuffer(bytesFromJSON("{\"testTag\": true}"));
-        mReader.pushTagMap(new HashMap<String, Reader.TagMapValue>() {{
-            put(TEST_TAG_JSON_KEY, new Reader.TagMapValue(TEST_TAG, false));
-        }});
-        assertEquals(TEST_TAG, mReader.readTag());
-        assertEquals(true, mReader.readBool());
+        Reader reader = getReader("{\"testTag\": true}", TAG_MAP);
+        assertEquals(TEST_TAG, reader.readTag());
+        assertEquals(true, reader.readBool());
     }
 
     @Test
     public void testReadUInt32() throws Exception {
-        mReader = ReaderJSON.fromBuffer(bytesFromJSON("{\"testTag\": 524}"));
-        mReader.pushTagMap(new HashMap<String, Reader.TagMapValue>() {{
-            put(TEST_TAG_JSON_KEY, new Reader.TagMapValue(TEST_TAG, false));
-        }});
-        assertEquals(TEST_TAG, mReader.readTag());
-        assertEquals(524, mReader.readUInt32());
+        Reader reader = getReader("{\"testTag\": 524}", TAG_MAP);
+        assertEquals(TEST_TAG, reader.readTag());
+        assertEquals(524, reader.readUInt32());
     }
 
     @Test
     public void testReadUInt64Under32Bits() throws Exception {
-        mReader = ReaderJSON.fromBuffer(bytesFromJSON("{\"testTag\": 524}"));
-        mReader.pushTagMap(new HashMap<String, Reader.TagMapValue>() {{
-            put(TEST_TAG_JSON_KEY, new Reader.TagMapValue(TEST_TAG, false));
-        }});
-        assertEquals(TEST_TAG, mReader.readTag());
-        assertEquals(524, mReader.readUInt64());
+        Reader reader = getReader("{\"testTag\": 524}", TAG_MAP);
+        assertEquals(TEST_TAG, reader.readTag());
+        assertEquals(524, reader.readUInt64());
     }
 
     @Test
     public void testReadUInt64() throws Exception {
-        mReader = ReaderJSON.fromBuffer(bytesFromJSON("{\"testTag\": 2886807498600235000}"));
-        mReader.pushTagMap(new HashMap<String, Reader.TagMapValue>() {{
-            put(TEST_TAG_JSON_KEY, new Reader.TagMapValue(TEST_TAG, false));
-        }});
-        assertEquals(TEST_TAG, mReader.readTag());
-        assertEquals(2886807498600235000L, mReader.readUInt64());
+        Reader reader = getReader("{\"testTag\": 2886807498600235000}", TAG_MAP);
+        assertEquals(TEST_TAG, reader.readTag());
+        assertEquals(2886807498600235000L, reader.readUInt64());
     }
 
     @Test
     public void testReadFloat32() throws Exception {
-        mReader = ReaderJSON.fromBuffer(bytesFromJSON("{\"testTag\": 12.24}"));
-        mReader.pushTagMap(new HashMap<String, Reader.TagMapValue>() {{
-            put(TEST_TAG_JSON_KEY, new Reader.TagMapValue(TEST_TAG, false));
-        }});
-        assertEquals(TEST_TAG, mReader.readTag());
-        assertEquals(12.24, mReader.readFloat32(), 0.001);
+        Reader reader = getReader("{\"testTag\": 12.24}", TAG_MAP);
+        assertEquals(TEST_TAG, reader.readTag());
+        assertEquals(12.24, reader.readFloat32(), 0.001);
     }
 
     @Test
     public void testReadFloat64() throws Exception {
-        mReader = ReaderJSON.fromBuffer(bytesFromJSON("{\"testTag\": 12.24}"));
-        mReader.pushTagMap(new HashMap<String, Reader.TagMapValue>() {{
-            put(TEST_TAG_JSON_KEY, new Reader.TagMapValue(TEST_TAG, false));
-        }});
-        assertEquals(TEST_TAG, mReader.readTag());
-        assertEquals(12.24, mReader.readFloat64(), 0.001);
+        Reader reader = getReader("{\"testTag\": 12.24}", TAG_MAP);
+        assertEquals(TEST_TAG, reader.readTag());
+        assertEquals(12.24, reader.readFloat64(), 0.001);
     }
 
     @Test
     public void testReadString() throws Exception {
-        mReader = ReaderJSON.fromBuffer(bytesFromJSON("{\"testTag\": \"h\u0121\u221A\"}"));
-        mReader.pushTagMap(new HashMap<String, Reader.TagMapValue>() {{
-            put(TEST_TAG_JSON_KEY, new Reader.TagMapValue(TEST_TAG, false));
-        }});
-        assertEquals(TEST_TAG, mReader.readTag());
-        assertEquals("h\u0121\u221A", mReader.readString());
+        Reader reader = getReader("{\"testTag\": \"h\u0121\u221A\"}", TAG_MAP);
+        assertEquals(TEST_TAG, reader.readTag());
+        assertEquals("h\u0121\u221A", reader.readString());
     }
 
     @Test
     public void testReadRepeatedString() throws Exception {
-        mReader = ReaderJSON.fromBuffer(bytesFromJSON("{\"testTagRepeated\":[\"string1\",\"string2\",\"string3\",\"string4\"]}"));
-        mReader.pushTagMap(new HashMap<String, Reader.TagMapValue>() {{
-            put(TEST_TAG_REPEATED_JSON_KEY, new Reader.TagMapValue(TEST_TAG_REPEATED, true));
-        }});
-        assertEquals(TEST_TAG_REPEATED, mReader.readTag());
-        assertEquals("string1", mReader.readString());
-        assertEquals(TEST_TAG_REPEATED, mReader.readTag());
-        assertEquals("string2", mReader.readString());
-        assertEquals(TEST_TAG_REPEATED, mReader.readTag());
-        assertEquals("string3", mReader.readString());
-        assertEquals(TEST_TAG_REPEATED, mReader.readTag());
-        assertEquals("string4", mReader.readString());
+        Reader reader = getReader("{\"testTagRepeated\":[\"string1\",\"string2\",\"string3\",\"string4\"]}", TAG_MAP_REPEATED);
+        assertEquals(TEST_TAG_REPEATED, reader.readTag());
+        assertEquals("string1", reader.readString());
+        assertEquals(TEST_TAG_REPEATED, reader.readTag());
+        assertEquals("string2", reader.readString());
+        assertEquals(TEST_TAG_REPEATED, reader.readTag());
+        assertEquals("string3", reader.readString());
+        assertEquals(TEST_TAG_REPEATED, reader.readTag());
+        assertEquals("string4", reader.readString());
     }
 
     @Test
     public void testNestedObject() throws Exception {
-        mReader = ReaderJSON.fromBuffer(bytesFromJSON("{\"testTag\":{\"testTagNested\":true},\"testTag2\":\"hello\"}"));
-        mReader.pushTagMap(new HashMap<String, Reader.TagMapValue>() {{
+        Reader reader = getReader("{\"testTag\":{\"testTagNested\":true},\"testTag2\":\"hello\"}", new HashMap<String, Reader.TagMapValue>() {{
             put(TEST_TAG_JSON_KEY, new Reader.TagMapValue(TEST_TAG, false));
             put(TEST_TAG_2_JSON_KEY, new Reader.TagMapValue(TEST_TAG_2, false));
         }});
-        assertEquals(TEST_TAG, mReader.readTag());
-        mReader.pushTagMap(new HashMap<String, Reader.TagMapValue>() {{
+        assertEquals(TEST_TAG, reader.readTag());
+        reader.pushTagMap(new HashMap<String, Reader.TagMapValue>() {{
             put(TEST_TAG_NESTED_JSON_KEY, new Reader.TagMapValue(TEST_TAG_NESTED, false));
         }});
-        assertEquals(TEST_TAG_NESTED, mReader.readTag());
-        assertEquals(true, mReader.readBool());
-        mReader.popTagMap();
-        assertEquals(TEST_TAG_2, mReader.readTag());
-        assertEquals("hello", mReader.readString());
+        assertEquals(TEST_TAG_NESTED, reader.readTag());
+        assertEquals(true, reader.readBool());
+        reader.popTagMap();
+        assertEquals(TEST_TAG_2, reader.readTag());
+        assertEquals("hello", reader.readString());
     }
 
     @Test
     public void testLimit() throws Exception {
-        mReader = ReaderJSON.fromBuffer(bytesFromJSON("{\"testTag\": 12.24}"));
-        mReader.pushTagMap(new HashMap<String, Reader.TagMapValue>() {{
-            put(TEST_TAG_JSON_KEY, new Reader.TagMapValue(TEST_TAG, false));
-        }});
-        int limit = mReader.pushLimit(0); // Should be a NO OP
-        assertEquals(TEST_TAG, mReader.readTag());
-        assertEquals(12.24, mReader.readFloat64(), 0.001);
-        mReader.popLimit(limit); // Should be a NO OP
+        Reader reader = getReader("{\"testTag\": 12.24}", TAG_MAP);
+        int limit = reader.pushLimit(0); // Should be a NO OP
+        assertEquals(TEST_TAG, reader.readTag());
+        assertEquals(12.24, reader.readFloat64(), 0.001);
+        reader.popLimit(limit); // Should be a NO OP
     }
 
     @Test
     public void testIncorrectTag() throws Exception {
-        mReader = ReaderJSON.fromBuffer(bytesFromJSON("{\"unknownTag\": 12.24}"));
-        mReader.pushTagMap(new HashMap<String, Reader.TagMapValue>() {{
-            put(TEST_TAG_JSON_KEY, new Reader.TagMapValue(TEST_TAG, false));
-        }});
-        assertEquals(0, mReader.readTag());
+        Reader reader = getReader("{\"unknownTag\": true}", TAG_MAP);
+        assertEquals(0, reader.readTag());
     }
 
     @Test
     public void testIncorrectByteValue() throws Exception {
-        mReader = ReaderJSON.fromBuffer(bytesFromJSON("{\"testTag\": true}"));
-        mReader.pushTagMap(new HashMap<String, Reader.TagMapValue>() {{
-            put(TEST_TAG_JSON_KEY, new Reader.TagMapValue(TEST_TAG, false));
-        }});
-        assertEquals(TEST_TAG, mReader.readTag());
-        assertEquals(0, mReader.readByte());
+        Reader reader = getReader("{\"testTag\": true}", TAG_MAP);
+        assertEquals(TEST_TAG, reader.readTag());
+        assertEquals(0, reader.readByte());
     }
 
     @Test
     public void testIncorrectVarIntValue() throws Exception {
-        mReader = ReaderJSON.fromBuffer(bytesFromJSON("{\"testTag\": true}"));
-        mReader.pushTagMap(new HashMap<String, Reader.TagMapValue>() {{
-            put(TEST_TAG_JSON_KEY, new Reader.TagMapValue(TEST_TAG, false));
-        }});
-        assertEquals(TEST_TAG, mReader.readTag());
-        assertEquals(0, mReader.readVarInt());
+        Reader reader = getReader("{\"testTag\": true}", TAG_MAP);
+        assertEquals(TEST_TAG, reader.readTag());
+        assertEquals(0, reader.readVarInt());
     }
 
     @Test
     public void testIncorrectVarUIntValue() throws Exception {
-        mReader = ReaderJSON.fromBuffer(bytesFromJSON("{\"testTag\": true}"));
-        mReader.pushTagMap(new HashMap<String, Reader.TagMapValue>() {{
-            put(TEST_TAG_JSON_KEY, new Reader.TagMapValue(TEST_TAG, false));
-        }});
-        assertEquals(TEST_TAG, mReader.readTag());
-        assertEquals(0, mReader.readVarUInt());
+        Reader reader = getReader("{\"testTag\": true}", TAG_MAP);
+        assertEquals(TEST_TAG, reader.readTag());
+        assertEquals(0, reader.readVarUInt());
     }
 
     @Test
     public void testIncorrectBoolValue() throws Exception {
-        mReader = ReaderJSON.fromBuffer(bytesFromJSON("{\"testTag\": \"not a bool\"}"));
-        mReader.pushTagMap(new HashMap<String, Reader.TagMapValue>() {{
-            put(TEST_TAG_JSON_KEY, new Reader.TagMapValue(TEST_TAG, false));
-        }});
-        assertEquals(TEST_TAG, mReader.readTag());
-        assertEquals(false, mReader.readBool());
+        Reader reader = getReader("{\"testTag\": \"not a bool\"}", TAG_MAP);
+        assertEquals(TEST_TAG, reader.readTag());
+        assertEquals(false, reader.readBool());
     }
 
     @Test
     public void testIncorrectUInt32Value() throws Exception {
-        mReader = ReaderJSON.fromBuffer(bytesFromJSON("{\"testTag\": true}"));
-        mReader.pushTagMap(new HashMap<String, Reader.TagMapValue>() {{
-            put(TEST_TAG_JSON_KEY, new Reader.TagMapValue(TEST_TAG, false));
-        }});
-        assertEquals(TEST_TAG, mReader.readTag());
-        assertEquals(0, mReader.readUInt32());
+        Reader reader = getReader("{\"testTag\": true}", TAG_MAP);
+        assertEquals(TEST_TAG, reader.readTag());
+        assertEquals(0, reader.readUInt32());
     }
 
     @Test
     public void testIncorrectUInt64Value() throws Exception {
-        mReader = ReaderJSON.fromBuffer(bytesFromJSON("{\"testTag\": true}"));
-        mReader.pushTagMap(new HashMap<String, Reader.TagMapValue>() {{
-            put(TEST_TAG_JSON_KEY, new Reader.TagMapValue(TEST_TAG, false));
-        }});
-        assertEquals(TEST_TAG, mReader.readTag());
-        assertEquals(0, mReader.readUInt64());
+        Reader reader = getReader("{\"testTag\": true}", TAG_MAP);
+        assertEquals(TEST_TAG, reader.readTag());
+        assertEquals(0, reader.readUInt64());
     }
 
     @Test
     public void testIncorrectFloat32Value() throws Exception {
-        mReader = ReaderJSON.fromBuffer(bytesFromJSON("{\"testTag\": true}"));
-        mReader.pushTagMap(new HashMap<String, Reader.TagMapValue>() {{
-            put(TEST_TAG_JSON_KEY, new Reader.TagMapValue(TEST_TAG, false));
-        }});
-        assertEquals(TEST_TAG, mReader.readTag());
-        assertEquals(0, mReader.readFloat32(), 0.001);
+        Reader reader = getReader("{\"testTag\": true}", TAG_MAP);
+        assertEquals(TEST_TAG, reader.readTag());
+        assertEquals(0, reader.readFloat32(), 0.001);
     }
 
     @Test
     public void testIncorrectFloat64Value() throws Exception {
-        mReader = ReaderJSON.fromBuffer(bytesFromJSON("{\"testTag\": true}"));
-        mReader.pushTagMap(new HashMap<String, Reader.TagMapValue>() {{
-            put(TEST_TAG_JSON_KEY, new Reader.TagMapValue(TEST_TAG, false));
-        }});
-        assertEquals(TEST_TAG, mReader.readTag());
-        assertEquals(0, mReader.readFloat64(), 0.001);
+        Reader reader = getReader("{\"testTag\": true}", TAG_MAP);
+        assertEquals(TEST_TAG, reader.readTag());
+        assertEquals(0, reader.readFloat64(), 0.001);
     }
 
     @Test
     public void testIncorrectStringValue() throws Exception {
-        mReader = ReaderJSON.fromBuffer(bytesFromJSON("{\"testTag\": true}"));
-        mReader.pushTagMap(new HashMap<String, Reader.TagMapValue>() {{
-            put(TEST_TAG_JSON_KEY, new Reader.TagMapValue(TEST_TAG, false));
-        }});
-        assertEquals(TEST_TAG, mReader.readTag());
-        assertEquals("", mReader.readString());
+        Reader reader = getReader("{\"testTag\": true}", TAG_MAP);
+        assertEquals(TEST_TAG, reader.readTag());
+        assertEquals("", reader.readString());
     }
 
     @Test
     public void testIncorrectRepeatedValue() throws Exception {
-        mReader = ReaderJSON.fromBuffer(bytesFromJSON("{\"testTagRepeated\": true}"));
-        mReader.pushTagMap(new HashMap<String, Reader.TagMapValue>() {{
-            put(TEST_TAG_REPEATED_JSON_KEY, new Reader.TagMapValue(TEST_TAG_REPEATED, true));
-        }});
-        assertEquals(0, mReader.readTag());
+        Reader reader = getReader("{\"testTagRepeated\": true}", TAG_MAP_REPEATED);
+        assertEquals(0, reader.readTag());
+    }
+
+    private Reader getReader(String json, Map<String, Reader.TagMapValue> map) {
+        Reader reader = ReaderJSON.fromBuffer(bytesFromJSON(json));
+        reader.pushTagMap(map);
+        return reader;
     }
 
     private byte[] bytesFromJSON(String json) {
