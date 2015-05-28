2cba9df873c51a38f360919d4a44d7bd494f1974
dutcher <edutcher@tonicdesign.com>
2015-05-28 11:38:26 -0400

    Added OSX Target

---
+++ swift/lib/ProtocGenSwift/ProtocGenSwift(OSX)/ProtocGenSwift(OSX).h
@@ -0,0 +1,19 @@
+//
+//  ProtocGenSwift(OSX).h
+//  ProtocGenSwift(OSX)
+//
+//  Created by Evan Dutcher on 5/28/15.
+//  Copyright (c) 2015 TonicDesign. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for ProtocGenSwift(OSX).
+FOUNDATION_EXPORT double ProtocGenSwift_OSX_VersionNumber;
+
+//! Project version string for ProtocGenSwift(OSX).
+FOUNDATION_EXPORT const unsigned char ProtocGenSwift_OSX_VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ProtocGenSwift_OSX_/PublicHeader.h>
+
+
