55d0cb0013c8badb4f74419589a8d45e8d419cbe
Jason Allum <jallum@tonicdesign.com>
2016-02-24 19:06:13 -0500

    Reworked using guard for clarity.

--- swift/lib/ProtocGenSwift/ProtocGenSwift/JSONReader.swift
+++ swift/lib/ProtocGenSwift/ProtocGenSwift/JSONReader.swift
@@ -32,21 +32,23 @@ public class JSONReader : Reader {
         }
         
         while let (key, value) = generator.next() {
-            if let (tag, repeated) = tagMap[key as! String] {
-                if repeated {
-                    if let array = value as? NSArray where array.count > 0 {
-                        repeatedObject = (key: tag, generator: array.generate())
-                        object = repeatedObject?.generator.next()
-                    } else {
-                        continue
-                    }
-                } else {
-                    object = value
-                }
-                return tag
-            } else {
+            guard let key = key as? String else {
                 continue
             }
+            guard let (tag, repeated) = tagMap[key] else {
+                continue
+            }
+            
+            if repeated {
+                guard let array = value as? NSArray where array.count > 0 else {
+                    continue
+                }
+                repeatedObject = (key: tag, generator: array.generate())
+                object = repeatedObject?.generator.next()
+            } else {
+                object = value
+            }
+            return tag
         }
         return 0
     }
